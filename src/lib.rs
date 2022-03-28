b44b8791b75e2665fd1c8f9cfa6386a5951af123
Igor Emelianov <cybercjp@gmail.com>
2022-03-28 17:17:57 +0300

    improvements

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,92 @@
 use rand::prelude::*;
-use std::cell::UnsafeCell;
+use std::alloc::{alloc, dealloc, Layout};
 use std::cmp::Ord;
-use std::mem::MaybeUninit;
+use std::mem;
+use std::ops::Index;
+use std::ops::IndexMut;
 use std::ptr::NonNull;
 
-const MAX_LEVEL: usize = 24;
+const MAX_LEVEL: usize = 20;
 
-struct Node<K, V> {
-    forward: [Option<NonNull<Node<K, V>>>; MAX_LEVEL],
+struct Tower<K, V> {
+    forward: [Option<NonNull<Node<K, V>>>; 0],
+}
+
+impl<K, V> Index<usize> for Tower<K, V> {
+    type Output = Option<NonNull<Node<K, V>>>;
+
+    fn index(&self, index: usize) -> &Option<NonNull<Node<K, V>>> {
+        unsafe { self.forward.get_unchecked(index) }
+    }
+}
+
+impl<K, V> IndexMut<usize> for Tower<K, V> {
+    fn index_mut(&mut self, index: usize) -> &mut Option<NonNull<Node<K, V>>> {
+        unsafe { self.forward.get_unchecked_mut(index) }
+    }
+}
+
+#[repr(C)]
+pub struct Node<K, V> {
     key: K,
     val: V,
+    layout: Layout,
+    tower: Tower<K, V>,
 }
 
 impl<K, V> Node<K, V> {
-    fn new(key: K, val: V) -> Self {
-        Self {
-            key,
-            val,
-            forward: [None; MAX_LEVEL],
+    pub fn alloc(height: usize) -> *mut Node<K, V> {
+        let size = mem::size_of::<K>()
+            + mem::size_of::<V>()
+            + mem::size_of::<Layout>()
+            + height * mem::size_of::<Option<NonNull<Node<K, V>>>>();
+        match Layout::from_size_align(size, 16) {
+            Ok(layout) => unsafe {
+                let ptr = alloc(layout) as *mut Node<K, V>;
+                if ptr.is_null() {
+                    return std::ptr::null_mut();
+                }
+                (*ptr).layout = layout;
+                for i in 0..height {
+                    (*ptr).tower[i] = None;
+                }
+                ptr
+            },
+            Err(why) => panic!("{}", why),
         }
     }
 
-    fn new_uninit() -> Self {
-        unsafe {
-            Self {
-                key: MaybeUninit::zeroed().assume_init(),
-                val: MaybeUninit::zeroed().assume_init(),
-                forward: [None; MAX_LEVEL],
-            }
+    pub fn new(key: K, val: V, height: usize) -> Option<NonNull<Node<K, V>>> {
+        let ptr = Node::alloc(height);
+        if ptr.is_null() {
+            return None;
         }
+        unsafe {
+            (*ptr).key = key;
+            (*ptr).val = val;
+        }
+        NonNull::new(ptr)
+    }
+
+    pub fn new_uninit(height: usize) -> Option<NonNull<Node<K, V>>> {
+        let ptr = Node::alloc(height);
+        if ptr.is_null() {
+            return None;
+        }
+        NonNull::new(ptr)
     }
 }
 
 fn rand_lvl() -> usize {
     let mut level = 1;
-    let branching = 2;
-    while level < MAX_LEVEL && (random::<usize>() % branching == 0) {
+    while level < MAX_LEVEL && (random::<usize>() % 2 == 0) {
         level += 1;
     }
     level
 }
 
 pub struct SkipList<K, V> {
-    head: UnsafeCell<Node<K, V>>,
+    head: NonNull<Node<K, V>>,
     size: usize,
     level: usize,
 }
@@ -50,7 +94,7 @@ pub struct SkipList<K, V> {
 impl<K: Ord, V> SkipList<K, V> {
     pub fn new() -> Self {
         Self {
-            head: UnsafeCell::new(Node::new_uninit()),
+            head: Node::new_uninit(MAX_LEVEL).unwrap(),
             size: 0,
             level: 1,
         }
@@ -58,44 +102,31 @@ impl<K: Ord, V> SkipList<K, V> {
 
     pub fn insert(&mut self, key: K, val: V) {
         let mut update: [Option<NonNull<Node<K, V>>>; MAX_LEVEL] = [None; MAX_LEVEL];
-        let mut x = NonNull::new(self.head.get_mut());
 
-        for i in (0..self.level).rev() {
-            unsafe {
-                while let Some(node_ptr) = x.unwrap().as_ref().forward[i] {
-                    if node_ptr.as_ref().key < key {
-                        x = x.unwrap().as_ref().forward[i];
-                    } else {
-                        break;
-                    }
+        unsafe {
+            if let Some(mut node_ptr) = self.find_gt_or_eq_node(&key, &mut update) {
+                let node = node_ptr.as_mut();
+                if node.key == key {
+                    node.val = val;
+                    return;
                 }
-                update[i] = x;
-            }
-        }
-
-        x = unsafe { x.unwrap().as_ref().forward[0] };
-        if let Some(mut node_ptr) = x {
-            let node = unsafe { node_ptr.as_mut() };
-            if node.key == key {
-                node.val = val;
-                return;
             }
         }
 
         let level = rand_lvl();
         if level > self.level {
             for i in self.level..level {
-                update[i] = NonNull::new(self.head.get_mut());
+                update[i] = Some(self.head);
             }
             self.level = level;
         }
 
-        x = NonNull::new(Box::into_raw(Box::new(Node::new(key, val))));
+        let x = Node::new(key, val, level);
 
         for i in 0..level {
             unsafe {
-                x.unwrap().as_mut().forward[i] = update[i].unwrap().as_ref().forward[i];
-                update[i].unwrap().as_mut().forward[i] = x;
+                x.unwrap().as_mut().tower[i] = update[i].unwrap().as_ref().tower[i];
+                update[i].unwrap().as_mut().tower[i] = x;
             }
         }
 
@@ -103,22 +134,30 @@ impl<K: Ord, V> SkipList<K, V> {
     }
 
     pub fn get_mut(&mut self, key: &K) -> Option<&mut V> {
+        let mut update: [Option<NonNull<Node<K, V>>>; MAX_LEVEL] = [None; MAX_LEVEL];
         unsafe {
-            if let Some(mut node_ptr) = self.find_node(key) {
-                Some(&mut node_ptr.as_mut().val)
-            } else {
-                None
+            if let Some(mut node_ptr) = self.find_gt_or_eq_node(key, &mut update) {
+                return if node_ptr.as_ref().key == *key {
+                    Some(&mut node_ptr.as_mut().val)
+                } else {
+                    None
+                };
             }
         }
+        None
     }
 
     pub fn get(&self, key: &K) -> Option<&V> {
+        let mut update: [Option<NonNull<Node<K, V>>>; MAX_LEVEL] = [None; MAX_LEVEL];
         unsafe {
-            if let Some(mut node_ptr) = self.find_node(key) {
-                Some(&node_ptr.as_mut().val)
-            } else {
-                None
+            if let Some(mut node_ptr) = self.find_gt_or_eq_node(key, &mut update) {
+                return if node_ptr.as_ref().key == *key {
+                    Some(&node_ptr.as_mut().val)
+                } else {
+                    None
+                };
             }
+            None
         }
     }
 
@@ -126,39 +165,37 @@ impl<K: Ord, V> SkipList<K, V> {
         self.size
     }
 
-    unsafe fn find_node(&self, key: &K) -> Option<NonNull<Node<K, V>>> {
-        let mut x = NonNull::new(self.head.get());
+    unsafe fn find_gt_or_eq_node(
+        &self,
+        key: &K,
+        update: &mut [Option<NonNull<Node<K, V>>>; MAX_LEVEL],
+    ) -> Option<NonNull<Node<K, V>>> {
+        let mut x = Some(self.head);
         for i in (0..self.level).rev() {
-            while let Some(node_ptr) = x.unwrap().as_ref().forward[i] {
+            while let Some(node_ptr) = x.unwrap().as_ref().tower[i] {
                 if node_ptr.as_ref().key < *key {
-                    x = x.unwrap().as_ref().forward[i];
+                    x = x.unwrap().as_ref().tower[i];
                 } else {
                     break;
                 }
             }
+            update[i] = x;
         }
 
-        x = x.unwrap().as_ref().forward[0];
-        if let Some(mut node_ptr) = x {
-            let node = node_ptr.as_mut();
-            if node.key == *key {
-                return x;
-            }
-        }
-
-        None
+        return x.unwrap().as_ref().tower[0];
     }
 }
 
 impl<K, V> Drop for SkipList<K, V> {
     fn drop(&mut self) {
-        let mut x = self.head.get_mut().forward[0];
-        while let Some(node_ptr) = x {
-            unsafe {
-                let t = node_ptr.as_ref().forward[0];
-                Box::from_raw(node_ptr.as_ptr());
+        unsafe {
+            let mut x = self.head.as_mut().tower[0];
+            while let Some(node_ptr) = x {
+                let t = node_ptr.as_ref().tower[0];
+                dealloc(node_ptr.as_ptr() as *mut u8, node_ptr.as_ref().layout);
                 x = t;
             }
+            dealloc(self.head.as_ptr() as *mut u8, self.head.as_ref().layout);
         }
     }
 }
